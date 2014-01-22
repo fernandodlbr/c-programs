ca723393df3e73a640851a758fb3e4f13c2364c0
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-01-22 10:23:01 +0100

    resubida

--- solutions/01/output.cpp
+++ solutions/01/output.cpp
@@ -1,9 +1,19 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
+#include <wctype.h>
 
 int main(int argc, char *argv[]){
 
-  printf("\n");
+  char frase[20];
 
-return EXIT_SUCCESS;
+  printf("Introduce tu frase: ");
+
+  scanf("%s", &*frase);
+
+//for (int contador=0; contador<20; contador++);
+
+  for(int repite=0; repite<10; repite++)
+    printf("%s\n", frase);
+
+  return EXIT_SUCCESS;
 }
