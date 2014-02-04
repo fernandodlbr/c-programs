7d16d376b1d709425b7a87f332c8b79004ffc210
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-02-04 11:23:50 +0100

    en proceso

---
+++ solutions/05/lista.cpp
@@ -0,0 +1,17 @@
+#include <stdio_ext.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+/*usar sizeof*/
+
+  const char *list[]={
+    "bread",
+    "toast",
+    "bacon"
+  };
+
+  printf("\n");
+
+  return EXIT_SUCCESS;
+}
