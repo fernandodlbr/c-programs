1298681e4ae2d299f437d16e207e4b77c6daaeed
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-02-04 10:09:43 +0100

    subida 2 completo

--- solutions/02/media.cpp
+++ solutions/02/media.cpp
@@ -1,9 +1,23 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
+#define notas 10
 
-int main(int argc, char *argv[]){
+  int main(){
 
-  printf("\n");
+  double nota,
+         media = 0;
 
-return EXIT_SUCCESS;
+ for(int alumno=0; alumno<notas; alumno+=1){
+
+    printf("nota ");
+
+    scanf(" %lf", &nota);
+
+    media+=nota;
+}
+
+   media/=notas;
+
+   printf("media: %.2lf\n", media);
+   return EXIT_SUCCESS;
 }
