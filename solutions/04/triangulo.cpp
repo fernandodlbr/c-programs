cc787f875d11773d379341a9ad2ca7e7f8d96ff5
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-02-11 09:41:23 +0100

    resubida 04 completo

--- solutions/04/triangulo.cpp
+++ solutions/04/triangulo.cpp
@@ -3,18 +3,16 @@
 
 int main(int argc, char *argv[]){
 
-int altura;
-printf("\nPon un numero para la altura deseada del triangulo: ");
-scanf("%i", &altura);
+  int altura;
+  printf("\nPon un numero para la altura deseada del triangulo: ");
+  scanf("%i", &altura);
 
   for(int lado=0; lado<altura; lado++){
-    printf("\n");
     for(int col=0; col<altura; col++)
       if(lado>=col) 
 	printf("*");
+
+    printf("\n"); 
   }
-
-  printf("\n"); 
-
   return EXIT_SUCCESS;
 }
