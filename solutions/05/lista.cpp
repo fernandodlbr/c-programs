14880110403ffd7e430a53d9e9f497246befc23f
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-02-19 11:19:58 +0100

    actualizacion txemistica

--- solutions/05/lista.cpp
+++ solutions/05/lista.cpp
@@ -4,14 +4,16 @@
 int main(int argc, char *argv[]){
 
 /*usar sizeof*/
-
+struct list{
   const char *list[]={
     "bread",
     "toast",
     "bacon"
   };
+};
+const char=sizeof(bread);
 
-  printf("\n");
+  printf("%s", list sizeof(struct list));
 
   return EXIT_SUCCESS;
 }
---
+++ solutions/11/imprimir.cpp
@@ -0,0 +1,11 @@
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char *argv[]){
+
+int strcmp (const char *list[]={"yiel", " yelp"});
+
+  printf("%s\n",list);
+
+return EXIT_SUCCESS;
+}
