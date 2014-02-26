f3a23d4a628d3a32b906b15bfcfb86d74e7118cd
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-02-26 09:43:20 +0100

    Subida 07 completa

--- solutions/07/double.cpp
+++ solutions/07/double.cpp
@@ -1,16 +1,26 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
 
-double add(double op1, double op2) {
+/* Este programa suma hasta dos numeros con dos decimales */
 
-  return op1 + op2
+double add(double op1, double op2){
+
+  return op1 + op2;
 }
 
 int main(int argc, char *argv[]){
 
   double param1, param2;
 
-  printf("%2.lf\n", add(param1, param2));
+  if (argc<0){
+
+    return EXIT_FAILURE;
+  }
+
+  param1 = atof(argv[1]);
+  param2 = atof(argv[2]);
+
+  printf("%.2lf\n", add(param1, param2));
 
   return EXIT_SUCCESS;
 }
