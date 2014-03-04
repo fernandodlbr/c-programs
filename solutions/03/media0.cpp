51af88066c4fb476e97fc68486db38bff1a19a5f
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-03-04 09:09:29 +0100

    subida 03 completa

--- solutions/03/media0.cpp
+++ solutions/03/media0.cpp
@@ -1,27 +1,25 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
-// #define notas 10
 
 int main(int argc, char *argv[]){
 
-  double nota,
-	 media = 0;
+  int nota,
+      contador;
+  double media = 0;
 
-  for(int alumno=0; alumno<'0'; alumno++){
+  do{
 
     printf("nota ");
+    scanf(" %i", &nota);
 
-    if(1,2,3,4,5,6,7,8,9,10)
-      // scanf(" %lf", &nota);
-    //  media/=notas;
+    if(nota>0){
+      media+=nota;
+      contador++;
+    }
 
-  //  else
-      scanf(" %lf", &nota);
+  }while(nota>0);
 
-    media+=nota;
-  }
-
-  //  media/=notas;
+  media/=contador;
   printf("media: %.2lf\n", media);
 
   return EXIT_SUCCESS;
