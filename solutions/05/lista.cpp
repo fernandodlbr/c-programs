01f5e5c75f94059075869ef609cf1171efd343e4
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-03-13 12:45:56 +0100

    resubida 05

--- solutions/05/lista.cpp
+++ solutions/05/lista.cpp
@@ -1,9 +1,14 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+//void print(const cahr *inicio){
 
-//  usar sizeof
+//  for(; *inicio != '\0'; inicio++)
+//    printf("%c\n", *inicio);
+
+//} Esto se haria con un valor print en el int main
+
+int main(int argc, char *argv[]){
 
   const char *list[]={
     "bread",
@@ -11,16 +16,16 @@ int main(int argc, char *argv[]){
     "bacon"
   };
 
-//  const int list_no = (sizeof list) / (sizeof list[0]);
+  // for(int cont=0; list[i] != NULL; cont++)  Si hay valor NULL en la const
 
-// for(int cont=0; list[i] != NULL; cont++)  Si hay valor NULL en la const
+  for(int cont=0; cont<sizeof(list) / sizeof(char *); cont++)
+    printf("%s \n", list[cont]);
 
-for(int cont=0; cont<sizeof(list) / sizeof(char *); cont++)
-  printf("%s \n", list[cont]);
+  //  const int list_no = (sizeof list) / (sizeof list[0]);
 
-//  printf("%s\n", *list);
-//  printf("%s\n", (*list)+6);
-//  printf("%s\n", (*list)+12);
+  //  printf("%s\n", *list);
+  //  printf("%s\n", (*list)+6);
+  //  printf("%s\n", (*list)+12);
 
   return EXIT_SUCCESS;
 }
