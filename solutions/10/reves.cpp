77a0bf49ac5ed085e4d34253f679b8bdabf6d43d
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-03-19 09:53:51 +0100

    subida 10 completa

--- solutions/10/reves.cpp
+++ solutions/10/reves.cpp
@@ -7,9 +7,18 @@ int main(int argc, char *argv[]){
   const char *beginning = sentence;
   const char *end = sentence;
 
-  /* Move end to the last byte in sentence*/ 
+  // mover el puntero hasta el final de la frase 
+  do{
+    *end++;
+  }while(*end != '\0');
 
-  printf("%s\n", end); 
+// reducir el puntero end hasta que sea igual que beginning
+  do{
+    end--;
+    printf("%c", *end); 
+  }while(*end != *beginning);
+
+  printf("\n");
 
   return EXIT_SUCCESS;
 }
