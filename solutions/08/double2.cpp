8545abed336c93f7026c1ea25844a6dee878c987
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-03-24 09:38:15 +0100

    Subida 08

--- solutions/08/double2.cpp
+++ solutions/08/double2.cpp
@@ -1,35 +1,16 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
 
-/* Este programa suma varios numeros con dos decimales */
-/* Metar un array para poder añadir n numeros de suma y un contador para
-guardar la suma anterior*/
-
-double add(double op1, double op2){
-
-  return op1 + op2;
-}
+// Este programa suma varios numeros con dos decimales
 
 int main(int argc, char *argv[]){
 
-  double param1, param2;
-
   double contador = 0;
-  double array[20];
 
-  for(int i=0; i<array; i++){
+  for(int argumento=1; argumento < argc; argumento++)
+    contador += atof(argv[argumento]);
 
-    if (argc<0){
-
-      return EXIT_FAILURE;
-    }
-
-}
-
-  param1 = atof(argv[1]);
-  param2 = atof(argv[2]);
-
-  printf("%.2lf\n", add(param1, param2));
+  printf("%.2lf\n", contador);
 
   return EXIT_SUCCESS;
 }
