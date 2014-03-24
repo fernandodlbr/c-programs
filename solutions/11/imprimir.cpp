c2d96f1ed8cd88925979d9fbe7c7a3e1c48e1f95
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-03-24 09:25:43 +0100

    Actualizacion

--- solutions/11/imprimir.cpp
+++ solutions/11/imprimir.cpp
@@ -1,9 +1,12 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
 #include <string.h>
+
 int main(int argc, char *argv[]){
 
 int strcmp (const char *list[]={"yiel", " yelp"});
+
+// el strcmp compara y ordena las palabras
 
   printf("%s\n",list);
 
