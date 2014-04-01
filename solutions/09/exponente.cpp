f448818883b9c7da854c7b428da692aed2afc49a
Fernando Diaz <fernanker@fernanker-pc.(none)>
2014-04-01 09:45:11 +0200

    actualizacion

--- solutions/09/exponente.cpp
+++ solutions/09/exponente.cpp
@@ -1,9 +1,27 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
 
+double power(double base, int exponent){
+
+  double result = 1;
+
+  for (int index = 0; index < exponent; index++)
+    result *= base;
+
+  return result;
+}
+
 int main(int argc, char *argv[]){
 
-  printf("\n");
+  double base, exponent;
 
-return EXIT_SUCCESS;
+  if (argc < 3)
+    return EXIT_FAILURE;
+
+  base = atof(argv[1]);
+  exponent = atoi(argv[2]);
+
+  printf("%lf\n", power(base, exponent));
+
+  return EXIT_SUCCESS;
 }
---
+++ solutions/13/primo.cpp
@@ -0,0 +1,26 @@
+#include <stdio_ext.h>
+#include <stdlib.h>
+
+#define MAX 70
+
+bool dividir(int dividendo, int posible_primo){
+  return !(dividendo % posible_primo);
+}
+
+int main(int argc, char *argv[]){
+
+  int primo[MAX];
+  int primos_encontrados = 1;
+  primo[0] = 2 ;
+
+/*
+mostrar 70 primos
+  dividir el posible primo entre los otros primos
+  añadir el nuevo primo a la lista de primos
+*/
+
+for(int candidato = 2; posible_primo<MAX; candidato++;)
+  bool posible_primo = true;
+    
+  return EXIT_SUCCESS;
+}
